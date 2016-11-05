d2271d42146f6492339b3edbb3c794b05c14e85d
Eugenio Pace <eugeniop@auth0.com>
2016-11-04 17:26:21 -0700

    Update awsiot.js

--- awsiot.js
+++ awsiot.js
@@ -45,7 +45,18 @@ server.get('/', (req, res, next) => {
           .toArray((err, result) => {
             db.close();
             if (err) return next(err);
-            res.status(200).send(_.map(result,(i) => {return {sn:i.serialNumber}}));
+            res.status(200).send(_.map(result,(i) => {
+                                                        var events = {
+                                                          "LONG": 'awake',
+                                                          "SINGLE": 'self awake',
+                                                          "DOUBLE": 'start sleep'
+                                                        };
+                                                        return {
+                                                                  sn:i.serialNumber,
+                                                                  ts:i.dateTime,
+                                                                  ev: events[i.clickType]
+                                                                };
+                                                      }));
     });
   });
 });
